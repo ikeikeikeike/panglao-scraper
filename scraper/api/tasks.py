351555bedb866e998b90a57ef2e4dab2e930a3b7
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-04-22 16:16:33 +0900

    modify download

--- scraper/api/tasks.py
+++ scraper/api/tasks.py
@@ -17,7 +17,7 @@ def download(url, opts=None):
     md5 = hashlib.md5(url.encode()).hexdigest()
     default_opts = {
         'outtmpl': '/tmp/{}.%(ext)s'.format(md5),
-        'progress_hooks': [lambda d: cache.set(url, d), lambda d: print(d)]
+        'progress_hooks': [lambda d: cache.set(url, d)]
     }
 
     opts = opts or {}
