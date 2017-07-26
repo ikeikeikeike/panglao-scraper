efb7b48b0dc9f063263c401f964335af97d14a27
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-07-26 23:44:22 +0900

    limit

--- scraper/cheapcdn/conv.py
+++ scraper/cheapcdn/conv.py
@@ -10,8 +10,8 @@ from core import error as core_error
 _ascii = string.ascii_uppercase + string.digits
 
 
-def _gen_mp3(src, dest):
-    return [
+def _gen_mp3(src, dest, empty=False):
+    cmd = [
         'ffmpeg',
         '-y',
         '-i', src,
@@ -19,6 +19,10 @@ def _gen_mp3(src, dest):
         '-qscale:a', '2',
         dest
     ]
+    if empty:
+        cmd.insert(4, '-t')
+        cmd.insert(5, '1')
+    return cmd
 
 
 def _gen_jpg(src, dest):
@@ -46,8 +50,8 @@ def _gen_preview(src, dest):
 
 class Media:
     EXTENTIONS = [
-        '.mp3',
         '.jpg',
+        '.mp3',
         '.preview.mp4',
     ]
 
@@ -83,8 +87,9 @@ class Media:
 
     def conv_mp3(self):
         dest, _ = os.path.splitext(self._filename)
+        empty = os.path.getsize(self._filename) > 104857600  # 100MByte
 
-        cmd = _gen_mp3(self._filename, f'{dest}.mp3')
+        cmd = _gen_mp3(self._filename, f'{dest}.mp3', empty)
         return subprocess.run(cmd).returncode
 
     def conv_jpg(self):
@@ -100,8 +105,10 @@ class Media:
         return subprocess.run(cmd).returncode
 
     def conv_all(self):
+        base, _ = os.path.splitext(self._filename)
         for ext in self.EXTENTIONS:
-            getattr(self, 'conv%s' % ext.replace('.', '_'))()
+            if not os.path.exists(f'{base}{ext}'):
+                getattr(self, 'conv%s' % ext.replace('.', '_'))()
 
     def filenames(self):
         base, _ = os.path.splitext(self._filename)
