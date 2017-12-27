51c37ed892f74d874d6d7106ce6df7a0cba71a9e
ikeikeikeike <jp.ne.co.jp@gmail.com>
2017-12-28 00:33:52 +0900

    api

--- scraper/lifecycle/views.py
+++ scraper/lifecycle/views.py
@@ -1,10 +1,8 @@
 from django import http
-from django.conf import settings
-from django.core.cache import caches
 
 from . import models
 
 
 def alives(request):
-    workers = models.Worker.objects.all()
-    return http.JsonResponse({'root': list(workers)})
+    qs = models.Worker.objects.filter(usable=True).values('name', 'host')
+    return http.JsonResponse({'root': list(qs)})
